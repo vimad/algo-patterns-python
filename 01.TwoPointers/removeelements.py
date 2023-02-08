318cd4e1d5169cbad97f4a7fb17929afc11ecb16
Vinod Madubashana <vinod.mby@gmail.com>
2023-02-08 21:37:12 +0530

    Two pointers | remove key in place by shifting key to right

---
+++ 01.TwoPointers/removeelements.py
@@ -0,0 +1,14 @@
+def remove_element(arr, key):
+    next_without_key = 0
+
+    for i in range(0, len(arr)):
+        if arr[i] != key:
+            arr[next_without_key] = arr[i]
+            next_without_key += 1
+    return arr
+
+if __name__ == '__main__':
+    print("Array new length: " +
+          str(remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3)))
+    print("Array new length: " +
+          str(remove_element([2, 11, 2, 2, 1], 2)))
